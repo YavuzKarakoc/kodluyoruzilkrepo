79685a26ec25285b399d62f66a2027984cbb8a2f
YavuzKarakoc <95426302+YavuzKarakoc@users.noreply.github.com>
2022-08-03 16:26:10 +0300

    Add files via upload

---
+++ SQL Ödev/Odev7.sql
@@ -0,0 +1,18 @@
+SELECT rating, COUNT(*) FROM film
+GROUP BY rating
+
+
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*)>50
+ORDER BY replacement_cost
+
+
+SELECT store_id , COUNT(*) FROM customer 
+GROUP BY store_id
+
+
+SELECT country_id , COUNT(*) FROM city 
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1
